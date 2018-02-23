f87810ccfc28767e8c9ec6a6a4f64c7fdbd9adee
Peter <peter.mcchau@gmail.com>
2018-02-23 22:15:50 +0800

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,29 @@
+#Algorithm file
+#
+#Assume there are 16 elements on a knot loop (loop_a)
+a = 'a'
+b = 'b'
+c = 'c'
+d = 'd'
+e = 'e'
+f = 'f'
+g = 'g'
+h = 'h'
+i = 'i'
+j = 'j'
+k = 'k'
+l = 'l'
+m = 'm'
+n = 'n'
+o = 'o'
+p = 'p'
+
+loop_a = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p]
+
+#The second knot loop also has 16 elements (loop_b)
+#
+#
+#
+#
+#
+#
